8a9ee8d0827acc2c8fac99cdd66342d68618d6f8
saharama <saharama@hawaii.edu>
2021-08-30 01:16:52 -1000

    Updates

---
+++ precache-manifest.543801fcb15d4165f1532087b6cd74f7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3318aca4326ac1f7140e67cf28b89be4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1ad727f3bfa834bccd",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "32d40dddc50d166e225c",
+    "url": "/static/css/main.b83db337.chunk.css"
+  },
+  {
+    "revision": "3a1ad727f3bfa834bccd",
+    "url": "/static/js/2.70e77121.chunk.js"
+  },
+  {
+    "revision": "4521ab2953843c8b91a1e91b7a90e37c",
+    "url": "/static/js/2.70e77121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d40dddc50d166e225c",
+    "url": "/static/js/main.88725ea9.chunk.js"
+  },
+  {
+    "revision": "45b7738852bae7280217",
+    "url": "/static/js/runtime-main.61217df4.js"
+  },
+  {
+    "revision": "4334612bf9e93ebb77940ec92db76555",
+    "url": "/static/media/CalyxBadon.4334612b.jpg"
+  },
+  {
+    "revision": "d61b36e7e9609d461953f35dcf3ebabf",
+    "url": "/static/media/LakotaNguyen.d61b36e7.jpg"
+  },
+  {
+    "revision": "15820dddaa73882d4ef934ec815a2bd4",
+    "url": "/static/media/MaxwellHiga.15820ddd.jpg"
+  },
+  {
+    "revision": "8b19b2a433b78aad929f276412e854b8",
+    "url": "/static/media/RooseveltFront.8b19b2a4.jpg"
+  },
+  {
+    "revision": "3e788c737029b79c503df9f4d883e172",
+    "url": "/static/media/donations_finishline.3e788c73.jpg"
+  },
+  {
+    "revision": "0098b5a6aef69aedcfe8c9a1c1841981",
+    "url": "/static/media/grouplei.0098b5a6.jpg"
+  },
+  {
+    "revision": "e5c7036f82b063074b94120040681c81",
+    "url": "/static/media/headshot-ogawa.e5c7036f.jpg"
+  },
+  {
+    "revision": "0a1869b62e4bd8354cce620bd801cc09",
+    "url": "/static/media/hoolauDinner.0a1869b6.jpg"
+  },
+  {
+    "revision": "7bdff94e2e80cc960daf861b95a190d2",
+    "url": "/static/media/kenekeGusman.7bdff94e.jpg"
+  },
+  {
+    "revision": "050db6e1ab3c14b52db33212970eb03c",
+    "url": "/static/media/landing2.050db6e1.jpg"
+  },
+  {
+    "revision": "327396d020651c9680b50e2a58ee3349",
+    "url": "/static/media/projectgrad-temp.327396d0.jpg"
+  },
+  {
+    "revision": "650d0b1aa73a5f0e40a4530cc2dbbea7",
+    "url": "/static/media/raf-80thAnni.650d0b1a.jpg"
+  },
+  {
+    "revision": "c11ad2d56dc3638bec30ca7bc6642e41",
+    "url": "/static/media/raf2.c11ad2d5.png"
+  },
+  {
+    "revision": "8cb887e2bba99bee9eb0ddec27f46af0",
+    "url": "/static/media/rhsaahawaii-1960mural.8cb887e2.jpg"
+  },
+  {
+    "revision": "45488cd70fa2241e9ed905c5251acd9d",
+    "url": "/static/media/rooseveltWhiteLogo.45488cd7.png"
+  },
+  {
+    "revision": "ddff84bdc4efad8e50229d5d5ac0621e",
+    "url": "/static/media/scholarships-2012.ddff84bd.jpg"
+  },
+  {
+    "revision": "b51ac9fbc778c001999bcec4fe9d8563",
+    "url": "/static/media/selfStorage.b51ac9fb.jpg"
+  },
+  {
+    "revision": "ade3478d29224f05659e03d39a35dbee",
+    "url": "/static/media/service-paintingmural.ade3478d.jpg"
+  }
+]);
